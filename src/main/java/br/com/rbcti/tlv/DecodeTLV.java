bf22f53ffba4663e0c2eb9dacd39a6d8542f8fcd
rbcunha79 <rbcunha@yahoo.com>
2023-06-27 08:39:17 -0300

    removed test methods

--- src/main/java/br/com/rbcti/tlv/DecodeTLV.java
+++ src/main/java/br/com/rbcti/tlv/DecodeTLV.java
@@ -1,10 +1,10 @@
 package br.com.rbcti.tlv;
 
+import static br.com.rbcti.tlv.ByteUtil.fromBigEndian;
 import static br.com.rbcti.tlv.TagTLV.BYTE_LENGTH_FLAG;
 import static br.com.rbcti.tlv.TagTLV.BYTE_LENGTH_MASK;
 import static br.com.rbcti.tlv.TagTLV.CONSTRUCTED_DATA_OBJECT;
 import static br.com.rbcti.tlv.TagTLV.SECOND_BYTE_TAG_NUMBER;
-import static br.com.rbcti.tlv.ByteUtil.fromBigEndian;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -150,62 +150,5 @@ public class DecodeTLV {
             }
         }
     }
-
-    public static void __main(String[] args) {
-
-        DecodeTLV decodeTLV = new DecodeTLV();
-        try {
-            //Com tamanho errado
-            List<TagTLV> tags = decodeTLV.decode(ByteUtil.decodeHex("61    14    4f 04 44 47 30 37 50 03 56 41 4c   5F 24 03 03 03 31"));
-
-            printTagTLV(tags, 2, '-');
-
-
-        } catch (DecodeTLVException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    public static void _main(String[] args) throws Exception {
-
-        DecodeTLV decodeTLV = new DecodeTLV(true);
-
-        //List<TagTLV>tags = decodeTLV.decode(ByteUtil.decodeHex("61 13   4F 05 F0 00 00 01 03    50 0A    4D 55 4C 54 4F 53 20 41 70 70    85 04 08 00 00 08  86 04 00 00 00 00 "));
-        //List<TagTLV>tags = decodeTLV.decode(ByteUtil.decodeHex(
-        //"6F 35 84 08 45 4F 50 43 43 41 52 44 A5 29 50 06 55 5A 4B 41 52 54 5F 2D 06 75 7A 72 75 65 6E 87 01 01 9F 11 01 01 9F 12 06 55 5A 4B 41 52 54 BF 0C 05 9F 4D 02 0B 0A"
-        //        ));
-
-        List<TagTLV>tags = decodeTLV.decode(ByteUtil.decodeHex("01 81 03 c1 c2 c3"));
-
-        tags = decodeTLV.getTags();
-        printTagTLV(tags, 2, '-');
-
-        // TagTLV tagTLV = findTagTLV(tags, TagTLVEnum.LOG_ENTRY.getId());
-
-        //System.out.println("::" + tagTLV);
-    }
-
-    public static void main(String[] args) {
-
-        final boolean STRICT_MODE = true;
-
-        DecodeTLV decodeTLV = new DecodeTLV(STRICT_MODE);
-
-        try {
-            List<TagTLV> tags = decodeTLV.decode(ByteUtil.decodeHex("6F 1E A5 1C 50 06 41 70 70 54 73 74 5F 2D 09 50 6F 72 74 75 67 75 65 73 BF 0C 05 9F 4D 02 0B 0A"));
-            DecodeTLV.printTagTLV(tags, 1, '-');
-
-            TagTLV logEntry = DecodeTLV.findTagTLV(tags, TagTLVEnum.LOG_ENTRY);
-            System.out.println("");
-            System.out.println(logEntry);
-
-        } catch (DecodeTLVException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
 
 }
